db00a97f651dada0274f3173a1dd46aa8c27d02c
Bryan Nguyen <bryanisaw350m3@gmail.com>
2023-10-03 23:21:57 -0700

    added post request to myApp

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -14,9 +14,14 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person) {
-    setCharacters([...characters, person]);
-  }
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   /* Note that this function does not return the data immediately—it instead returns a Promise. 
   Promises are useful when we need to perform an operation which will take some time to finish, 
@@ -27,14 +32,30 @@ function MyApp() {
     return promise;
   }
 
-  useEffect(() => {
-    fetchUsers()
-      .then((res) => res.json())
-      .then((json) => setCharacters(json["users_list"]))
+  /* In this function, note that we're calling fetch with the same route, but passing some additional 
+  options: method to make it a POST instead of the default GET, body to put the person data into the 
+  body of the request, and a Content-Type header to tell the server that the body contains a JSON-formatted 
+  object. We also use the Javascript built-in JSON.stringify to convert, or serialize, the in-memory object 
+  to a string which we can send "over the wire". */
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+
+    return promise;
+  }
+
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }
 
   return (
     <div className="container">
